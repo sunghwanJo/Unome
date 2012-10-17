d9dff3248b371af9bd54b50448193ac14a5e8d41
luman <namul10@gmail.com>
2012-10-17 22:16:11 +0900

    add analyzer tool

---
+++ jhannanum.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os.path, jpype
+
+jarpath = os.path.join(os.path.abspath('.'), 'jhannanum.jar')
+jpype.startJVM(jpype.getDefaultJVMPath(),"-ea","-Djava.class.path=%s"%jarpath)
+
+
+WorkflowCls = jpype.JClass('kr.ac.kaist.swrc.jhannanum.hannanum.Workflow')
+WorkflowFactoryCls = jpype.JClass('kr.ac.kaist.swrc.jhannanum.hannanum.WorkflowFactory')
+
+
+class WorkflowMorphAnalyzer:
+    operation_flag = False
+    workflow = WorkflowFactoryCls.getPredefinedWorkflow(WorkflowFactoryCls.WORKFLOW_MORPH_ANALYZER)
+
+    def operation_analyzer(self):  
+        try:
+            self.workflow.activateWorkflow(True)
+            self.operation_flag = True
+            print 'analyze start'
+        except :
+            print 'exception !!'
+            exit()
+
+    def exit_analyzer(self):
+        self.operation_flag = False
+        self.workflow.close()
+
+    def request_analyze(self,plaintext):
+        if self.operation_flag == True:
+            self.workflow.analyze(plaintext)
+            print self.workflow.getResultOfDocument()
+
+
--- unome_utils.py
+++ unome_utils.py
@@ -1,4 +1,11 @@
-#
+from jpype import *
+
+class jhannanum:
+    def __init__(self, jvm_path, class_path):
+        startJVM(getDefaultJVMPath())
+        
+
+
 
 def analyze_tweet()
     pass
