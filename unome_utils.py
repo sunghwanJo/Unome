b7e00a6131c83c39e4c772fb7210ec5969689626
root <root@fd2476b5-f0c5-4af4-aec7-d97cb0897058.cs1981cloud.internal>
2012-11-18 00:15:35 +0900

    * 정리용도

--- unome_utils.py
+++ unome_utils.py
@@ -13,13 +13,14 @@ class TweetAnalyzer(WorkflowMorphAnalyzer):
     def __init__(self):
         WorkflowMorphAnalyzer.__init__(self)
 
-    def get_emotion_point(self, string):
+    def get_emotion_data(self, string):
         analyzed_table = {}
         
         if self._get_operation_flag() == False:
             return False
         string_sequence = string.split(' ')
         seperated_list = self.request_analyze(string).split('\n')
+        
         for word in seperated_list:
             word = word.replace('\n', '')
             if not len(word):
@@ -33,8 +34,11 @@ class TweetAnalyzer(WorkflowMorphAnalyzer):
         for key in analyzed_table.keys():
             for value in analyzed_table[key]:
                 emotion = self.compare_with_emotiontable(value)
-                if emotion:
+                #감정을 찾으면?  string_sequence.index(key), emotion
+                if emotion:  
                     print emotion, string_sequence.index(key), key
+
+
                     break  # analyzed_table[key]의 첫번째  value만 탐색i
 
         return True
@@ -45,11 +49,11 @@ class TweetAnalyzer(WorkflowMorphAnalyzer):
         db.session.add(emotiontable)
         db.session.commit()
 
-    def compare_with_emotiontable(self, word):
+    def compare_with_emotiontable(self, value):
         emotion_table = EmotionTable.query.all()
         emotion = ''
         for emotion_object in emotion_table:
-            if emotion_object.value in word:
+            if emotion_object.value in value:
                 emotion = emotion_object.emotion
                 break
         if emotion:
@@ -58,13 +62,17 @@ class TweetAnalyzer(WorkflowMorphAnalyzer):
         return False
 
 
-    def compare_with_amptable(self, word):
+    def compare_with_amptable(self, value):
         amp_point = 1
         return amp_point
 
-    def compare_with_changetable(self, word):
-        change_flag = False
+    def compare_with_changetable(self, value):
+        change_flag = 1
+
         return change_flag
+
+
+    
 
 
 
