a5f2521f15142cbcea0738204f196c5e17a0cbd8
sunghwanJo <namul10@gmail.com>
2012-11-18 00:46:07 +0900

    * remove file relative web Interface

--- models.py
+++ models.py
@@ -3,21 +3,6 @@ from unome import app
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    name = db.Column(db.String(60))
-
-    oauth_token = db.Column(db.String(200))
-    oauth_secret = db.Column(db.String(200))
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return '<Users %r>'%self.name
-
-
 class EmotionTable(db.Model):
     __tablename__ = 'EmotionTable'
     id = db.Column('analyzed_id', db.Integer, primary_key=True)
